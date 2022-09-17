f54c1b689f8dee4ffd8f21f4779fc0dff09831d4
Artem Rudenko <phomdev@yandex.ru>
2022-09-17 13:51:23 +0300

    fix: редакторинг после первой итерации

--- scripts/index.js
+++ scripts/index.js
@@ -3,33 +3,37 @@ const editProfile = document.querySelector('.profile__editor');
 // Получаем popup редактирования профиля
 const popupEditProfile = document.querySelector('.popup-edit');
 // Получаем элемент иконки закрытия popup
-const popupClose = popupEditProfile.querySelector('.popup-edit__close');
+const popupCloseIcon = popupEditProfile.querySelector('.popup-edit__close');
 // Получаем имя профиля
 const profileName = document.querySelector('.profile__name');
 // Получаем описание профиля
 const profileDescription = document.querySelector('.profile__description');
 // Получаем input имени
-const nameInput = popupEditProfile.querySelector('.popup-edit__input_name');
+const nameInput = popupEditProfile.querySelector('.popup-edit__input_item_name');
 // Получаем input описания
-const descriptionInput = popupEditProfile.querySelector('.popup-edit__input_description');
+const descriptionInput = popupEditProfile.querySelector('.popup-edit__input_item_description');
 
-// Функция для добавления класса в popup
-const popupToggle = function () {
-  popupEditProfile.classList.toggle('popup-edit_opened');
+// Функция открытия popup
+const popupOpen = function () {
+  popupEditProfile.classList.add('popup-edit_opened');
   nameInput.value = profileName.textContent;
   descriptionInput.value = profileDescription.textContent;
+}
+// Функция закрытия popup
+const popupClose = function () {
+  popupEditProfile.classList.remove('popup-edit_opened');
 }
 // Функция сохранения введённых в форму данных (имени и описания)
 const formSubmitHandler = function (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileDescription.textContent = descriptionInput.value;
-  popupToggle();
+  popupClose();
 }
 
 // Добавляем класс в popup при клике на иконку редактирования профиля
-editProfile.addEventListener('click', popupToggle);
+editProfile.addEventListener('click', popupOpen);
 // Удаляем класс из popup при клике на иконку закрытия popup
-popupClose.addEventListener('click', popupToggle);
+popupCloseIcon.addEventListener('click', popupClose);
 // Обновляем данные формы при нажатии кнопки сохранения
 popupEditProfile.addEventListener('submit', formSubmitHandler);
